766881936890567999f68079cb21e582eb75496e
rain <rainbow891021@gmail.com>
2026-04-28 10:12:46 +0800

    Fix error for Dashboard show

--- frontend/dist/assets/index-DP3_w7UR.js
+++
@@ -1,1 +0,0 @@
-import{b3 as o,V as a,b4 as r}from"./index-6_Jov0EN.js";function b(){r({variableName:a("scrollbar.width").name})}function c(){o({variableName:a("scrollbar.width").name})}export{b,c as u};
---
+++ frontend/dist/assets/index-h6hpsB9x.js
@@ -0,0 +1,1 @@
+import{b3 as o,V as a,b4 as r}from"./index-D_XfuvxA.js";function b(){r({variableName:a("scrollbar.width").name})}function c(){o({variableName:a("scrollbar.width").name})}export{b,c as u};
